a66ea85f00e67f789e402de3333c8bde79566c88
moxir1998 <122140991+moxir1998@users.noreply.github.com>
2024-12-08 10:31:56 +0500

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -12,6 +12,6 @@ TelegramBot = Client(
     name="bot",
     api_id=Telegram.28136820,
     api_hash=Telegram.20eb6e8833f0d5c89e2e7a784b2693bc,
-    bot_token=Telegram.7722662123:AAGfvuz0Hcs4Cln2mUsw-o9BXIo-R3t0ADk,
+    bot_token=Telegram.7712627407:AAEM9glTY6AwP1gzNT2y_m_BIPEro5WfI8M,
     plugins={'root': 'bot/plugins'}
 )
